07fce0f7c951d8f2f3407866b6e9e32e9c1295be
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-04-18 02:48:10 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+  import "@skeletonlabs/skeleton/styles/all.css";
+  import "./app.postcss";
+</script>
+
+<button class="btn variant-filled-surface"> Hello World </button>
